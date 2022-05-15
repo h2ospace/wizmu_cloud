a04b4ce2dfac5c1bd88c2593509988ae469b0489
TANIGUCHI Makoto <master@h2o-space.com>
2022-05-16 00:14:21 +0900

    流れのHTMLを制作

---
+++ done.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>done</title>
+</head>
+<body>
+<div style="position: relative; padding-top: 56.25%;"><iframe src="https://iframe.videodelivery.net/42edcd6fe5854c39877b6e638c6e00f8?poster=https%3A%2F%2Fvideodelivery.net%2F42edcd6fe5854c39877b6e638c6e00f8%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600" style="border: none; position: absolute; top: 0; left: 0; height: 100%; width: 100%;" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true"></iframe></div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<body>
+<form action="done.php">
+    <dl>
+        <dt>動画ファイル</dt>
+        <dd><input type="file" name="movie"></dd>
+    </dl>
+    <button type="submit">アップロード</button>
+</form>
+</body>
+</html>
